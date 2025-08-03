7f545081e7569d9e317f0ba4f716ba2c524797d0
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-03 18:30:36 +0100

    App.js:

---
+++ famepropertiesng/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import './css/style.css';
+import { Index } from './components';
+import { Cart } from './components/cart';
+import { Checkout } from './components/checkout';
+import { Contact } from './components/contact';
+import { Detail } from './components/detail';
+import { Shop } from './components/shop';
+
+function App() {
+  return (
+    <>
+      <body>
+        <Index />
+        {/* <Cart /> */}
+        {/* <Checkout /> */}
+        {/* <Contact /> */}
+        {/* <Detail /> */}
+        {/* <Shop /> */}
+      </body>
+    </>
+  );
+}
+
+export default App;
