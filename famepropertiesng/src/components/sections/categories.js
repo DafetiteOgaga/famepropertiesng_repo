5409a810a44c33850cccbdff7a5f1db60db9f25c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-05 14:37:42 +0100

    categories.js: reactified the component to dynamically render multiple component using map()

--- famepropertiesng/src/components/sections/categories.js
+++ famepropertiesng/src/components/sections/categories.js
@@ -1,166 +1,41 @@
 
 
+const productArr = [
+	"cat-1.jpg",
+	"cat-2.jpg",
+	"cat-3.jpg",
+	"cat-4.jpg",
+	"cat-2.jpg",
+	"cat-3.jpg",
+	"cat-4.jpg",
+	"cat-1.jpg",
+	"cat-3.jpg",
+	"cat-4.jpg",
+	"cat-1.jpg",
+	"cat-2.jpg",
+]
 function Categories({getImage}) {
 	return (
 		<div className="container-fluid pt-5">
 			<h2 className="section-title position-relative text-uppercase mx-xl-5 mb-4"><span className="bg-secondary pr-3">Categories</span></h2>
 			<div className="row px-xl-5 pb-3">
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-1.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
+				{productArr.map((product, index) => {
+					return (
+						<div key={index} className="col-lg-3 col-md-4 col-sm-6 pb-1">
+							<a className="text-decoration-none" href="##">
+								<div className="cat-item img-zoom d-flex align-items-center mb-4">
+									<div className="overflow-hidden categories-div">
+										<img className="img-fluid" alt="" src={getImage(product)}/>
+									</div>
+									<div className="flex-fill pl-3">
+										<h6>Category Name</h6>
+										<small className="sec-text-body">100 Products</small>
+									</div>
+								</div>
+							</a>
 						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-2.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-3.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-4.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-4.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-3.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-2.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-1.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-2.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-1.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-4.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div className="col-lg-3 col-md-4 col-sm-6 pb-1">
-					<a className="text-decoration-none" href="##">
-						<div className="cat-item img-zoom d-flex align-items-center mb-4">
-							<div className="overflow-hidden categories-div">
-								<img className="img-fluid" alt="" src={getImage("cat-3.jpg")}/>
-							</div>
-							<div className="flex-fill pl-3">
-								<h6>Category Name</h6>
-								<small className="text-body">100 Products</small>
-							</div>
-						</div>
-					</a>
-				</div>
+					)
+				})}
 			</div>
 		</div>
 	)
