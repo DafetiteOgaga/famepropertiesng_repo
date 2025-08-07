75583911f23130dc9aca8fbe2c8ae0766523dbdc
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-07 13:14:32 +0100

    cart.js: removed commented codes

--- famepropertiesng/src/components/cart.js
+++ famepropertiesng/src/components/cart.js
@@ -114,11 +114,6 @@ function Cart() {
 										:{}}>{head}</th>
 									)
 								})}
-								{/* <th>Products</th>
-								<th>Price</th>
-								<th>Quantity</th>
-								<th>Total</th>
-								<th>Remove</th> */}
 							</tr>
 						</thead>
 						<tbody className="align-middle">
@@ -159,111 +154,6 @@ function Cart() {
 									</tr>
 								)
 							})}
-							{/* <tr>
-								<td className="align-middle"><img src={getImage("product-1.jpg")} alt="" className="cart-image-img"/> Product Name</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle">
-									<div className="input-group quantity mx-auto cart-td-table">
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-minus" >
-											<i className="fa fa-minus"></i>
-											</button>
-										</div>
-										<input type="text" className="form-control form-control-sm bg-secondary border-0 text-center" value="1"/>
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-plus">
-												<i className="fa fa-plus"></i>
-											</button>
-										</div>
-									</div>
-								</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle"><button className="btn btn-sm btn-danger"><i className="fa fa-times"></i></button></td>
-							</tr>
-							<tr>
-								<td className="align-middle"><img src={getImage("product-2.jpg")} alt="" className="cart-image-img"/> Product Name</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle">
-									<div className="input-group quantity mx-auto cart-td-table">
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-minus" >
-											<i className="fa fa-minus"></i>
-											</button>
-										</div>
-										<input type="text" className="form-control form-control-sm bg-secondary border-0 text-center" value="1"/>
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-plus">
-												<i className="fa fa-plus"></i>
-											</button>
-										</div>
-									</div>
-								</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle"><button className="btn btn-sm btn-danger"><i className="fa fa-times"></i></button></td>
-							</tr>
-							<tr>
-								<td className="align-middle"><img src={getImage("product-3.jpg")} alt="" className="cart-image-img"/> Product Name</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle">
-									<div className="input-group quantity mx-auto cart-td-table">
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-minus" >
-											<i className="fa fa-minus"></i>
-											</button>
-										</div>
-										<input type="text" className="form-control form-control-sm bg-secondary border-0 text-center" value="1"/>
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-plus">
-												<i className="fa fa-plus"></i>
-											</button>
-										</div>
-									</div>
-								</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle"><button className="btn btn-sm btn-danger"><i className="fa fa-times"></i></button></td>
-							</tr>
-							<tr>
-								<td className="align-middle"><img src={getImage("product-4.jpg")} alt="" className="cart-image-img"/> Product Name</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle">
-									<div className="input-group quantity mx-auto cart-td-table">
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-minus" >
-											<i className="fa fa-minus"></i>
-											</button>
-										</div>
-										<input type="text" className="form-control form-control-sm bg-secondary border-0 text-center" value="1"/>
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-plus">
-												<i className="fa fa-plus"></i>
-											</button>
-										</div>
-									</div>
-								</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle"><button className="btn btn-sm btn-danger"><i className="fa fa-times"></i></button></td>
-							</tr>
-							<tr>
-								<td className="align-middle"><img src={getImage("product-5.jpg")} alt="" className="cart-image-img"/> Product Name</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle">
-									<div className="input-group quantity mx-auto cart-td-table">
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-minus" >
-											<i className="fa fa-minus"></i>
-											</button>
-										</div>
-										<input type="text" className="form-control form-control-sm bg-secondary border-0 text-center" value="1"/>
-										<div className="input-group-btn">
-											<button className="btn btn-sm btn-primary btn-plus">
-												<i className="fa fa-plus"></i>
-											</button>
-										</div>
-									</div>
-								</td>
-								<td className="align-middle">$150</td>
-								<td className="align-middle"><button className="btn btn-sm btn-danger"><i className="fa fa-times"></i></button></td>
-							</tr> */}
 						</tbody>
 					</table>
 				</div>
