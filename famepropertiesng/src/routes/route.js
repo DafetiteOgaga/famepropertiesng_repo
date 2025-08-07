16274d6c20f1d9d9840ac02553d420f859880154
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-07 02:38:37 +0100

    route.js: handles routing

---
+++ famepropertiesng/src/routes/route.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Contact } from '../components/contact';
+import { Home } from '../components/home';
+import { Index } from '../components';
+
+function AppRoutes() {
+	return (
+		<Routes>
+			<Route path="/" element={<Index />}>
+				{/* This makes Home the default route */}
+				<Route index element={<Home />} />
+				
+				{/* Other routes will replace <Outlet /> */}
+				<Route path="contact" element={<Contact />} />
+			</Route>
+		</Routes>
+	);
+}
+
+export {AppRoutes};
