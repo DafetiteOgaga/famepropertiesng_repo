9182cec59f603ed422a865728ea2442fa1bd939a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-08 13:01:25 +0100

    checkout.js: 				<Route path="products/:productname" element={<Products />} />q

--- famepropertiesng/src/components/checkout.js
+++ famepropertiesng/src/components/checkout.js
@@ -147,31 +147,31 @@ function Checkout() {
 								<h6 className="mb-3">Products</h6>
 								<div className="d-flex justify-content-between">
 									<p>Product Name 1</p>
-									<p>$150</p>
+									<p>₦150</p>
 								</div>
 								<div className="d-flex justify-content-between">
 									<p>Product Name 2</p>
-									<p>$150</p>
+									<p>₦150</p>
 								</div>
 								<div className="d-flex justify-content-between">
 									<p>Product Name 3</p>
-									<p>$150</p>
+									<p>₦150</p>
 								</div>
 							</div>
 							<div className="border-bottom pt-3 pb-2">
 								<div className="d-flex justify-content-between mb-3">
 									<h6>Subtotal</h6>
-									<h6>$150</h6>
+									<h6>₦150</h6>
 								</div>
 								<div className="d-flex justify-content-between">
 									<h6 className="font-weight-medium">Shipping</h6>
-									<h6 className="font-weight-medium">$10</h6>
+									<h6 className="font-weight-medium">₦10</h6>
 								</div>
 							</div>
 							<div className="pt-2">
 								<div className="d-flex justify-content-between mt-2">
 									<h5>Total</h5>
-									<h5>$160</h5>
+									<h5>₦160</h5>
 								</div>
 							</div>
 						</div>
