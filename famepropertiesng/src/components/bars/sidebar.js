3937b79b8a97befd22a6d6da8f49400b9bf0bd61
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-12 00:57:09 +0100

    sidebar.js: fix for responsiveness

--- famepropertiesng/src/components/bars/sidebar.js
+++ famepropertiesng/src/components/bars/sidebar.js
@@ -34,7 +34,7 @@ function Sidebar({mobileStyle = null, categoryMenuRef = null}) {
 			padding: mobileStyle?'1.2rem 1rem':'',
 			overflowY: 'auto',
 			overflowX: 'hidden',
-			maxHeight: '70vh',
+			maxHeight: mobileStyle?'70vh':'',
 			borderTopLeftRadius: mobileStyle?'0.8rem':'',
 			borderBottomLeftRadius: mobileStyle?'0.8rem':'',
 			// backgroundColor: '#000',
