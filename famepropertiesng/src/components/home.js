e03c3b7b6b22b6f536cae84f4d65275b8c0e70da
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-12 19:21:54 +0100

    home.js: switched from using images locally to those stored on github

--- famepropertiesng/src/components/home.js
+++ famepropertiesng/src/components/home.js
@@ -1,13 +1,13 @@
 import { Carousel } from "./sections/carousel"
 import { Products } from "./sections/products"
 
-const images = require.context('../images/img', false, /\.(png|jpe?g|svg)$/);
-const getImage = (name) => (images(`./${name}`)) // to get a specific image by name
+// const images = require.context('../images/img', false, /\.(png|jpe?g|svg)$/);
+// const getImage = (name) => (images(`./${name}`)) // to get a specific image by name
 function Home() {
 	return (
 		<>
-			<Carousel getImage={getImage} />
-			<Products getImage={getImage} />
+			<Carousel />
+			<Products />
 		</>
 	)
 }
