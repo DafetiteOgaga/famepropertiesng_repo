09338fa62fece4eaf62a555b1dd4b9b3cc84581d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-29 22:11:34 +0100

    App.js: minor fix

--- famepropertiesng/src/App.js
+++ famepropertiesng/src/App.js
@@ -4,7 +4,7 @@ import './css/style.css';
 import './css/responsive.css';
 import './css/animation.css';
 // import './css/animations.css';
-import './css/reduced-motion.css'
+import './css/reduced-motion.css';
 // import './css/test.css'
 // import { useLogMediaSize } from './hooks/mediaSize';
 import { AppRoutes } from './routes/route';
