5eceaef78faf8fbfb31d16f0de1ae656737dd806
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-31 01:32:01 +0100

    dynamicFetchSetup.js: fetches from the server

---
+++ famepropertiesng/src/components/loginSignUpProfile/dynamicFetchSetup.js
@@ -0,0 +1,21 @@
+import { toast } from "react-toastify";
+import { getBaseURL } from "../../hooks/fetchAPIs";
+
+const baseURL = getBaseURL();
+const authenticator = async () => {
+	try {
+		const response = await fetch(`${baseURL}/imagekit-auth/`);
+		if (!response.ok) {
+			const errorText = "Failed to authenticate with ImageKit"
+			toast.error(errorText);
+			throw new Error(errorText);
+		}
+		const data = await response.json();
+		// console.log("Authentication data received");
+		// console.log("Authentication data:", data);
+		return data;
+	} catch (error) {
+		throw new Error(`Authentication failed: ${error.message}`);
+	}
+};
+export { authenticator };
