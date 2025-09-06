48ad0f0a21996d3fadc25e9c1cf2a4eed72f5019
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-06 10:14:35 +0100

    pagination.js: set the buttons to dynamically render only when total pages is gt 1

--- famepropertiesng/src/hooks/pagination.js
+++ famepropertiesng/src/hooks/pagination.js
@@ -20,72 +20,75 @@ function Pagination({ pagination, onPageChange }) {
 	// });
 
 	return (
-		<ul className="pagination">
-			{/* Previous */}
-			<li className={`page-item ${!prev ? "disabled" : ""}`}>
-				<button
-				className="page-link"
-				disabled={!prev}
-				style={{
-					borderTopLeftRadius: "1rem",
-					borderBottomLeftRadius: "1rem",
-					borderTopRightRadius: "0.35rem",
-					borderBottomRightRadius: "0.35rem",
-				}}
-				onClick={() => prevPage && onPageChange(prevPage)}
-				>
-					Prev
-				</button>
-			</li>
+		<>
+			{total_pages>1&&
+			<ul className="pagination">
+				{/* Previous */}
+				<li className={`page-item ${!prev ? "disabled" : ""}`}>
+					<button
+					className="page-link"
+					disabled={!prev}
+					style={{
+						borderTopLeftRadius: "1rem",
+						borderBottomLeftRadius: "1rem",
+						borderTopRightRadius: "0.35rem",
+						borderBottomRightRadius: "0.35rem",
+					}}
+					onClick={() => prevPage && onPageChange(prevPage)}
+					>
+						Prev
+					</button>
+				</li>
 
-			{/* Current page (derived from prevPage + 1) */}
-			<Fragment>
-				<li className="page-item active">
-					<button className="page-link"
+				{/* Current page (derived from prevPage + 1) */}
+				<Fragment>
+					<li className="page-item active">
+						<button className="page-link"
+						style={{
+							borderTopLeftRadius: "0.35rem",
+							borderBottomLeftRadius: "0.35rem",
+							borderTopRightRadius: "0.35rem",
+							borderBottomRightRadius: "0.35rem",
+						}}>
+							{/* {console.log('prevPage', prevPage)} */}
+							{prevPage ? prevPage + 1 : 1}
+						</button>
+					</li>
+				</Fragment>
+
+				{/* Next */}
+				<li className={`page-item ${!next ? "disabled" : ""}`}>
+					<button
+					className="page-link"
+					disabled={!next}
 					style={{
 						borderTopLeftRadius: "0.35rem",
 						borderBottomLeftRadius: "0.35rem",
 						borderTopRightRadius: "0.35rem",
 						borderBottomRightRadius: "0.35rem",
-					}}>
-						{/* {console.log('prevPage', prevPage)} */}
-						{prevPage ? prevPage + 1 : 1}
+					}}
+					onClick={() => nextPage && onPageChange(nextPage)}
+					>
+						Next
 					</button>
 				</li>
-			</Fragment>
 
-			{/* Next */}
-			<li className={`page-item ${!next ? "disabled" : ""}`}>
-				<button
-				className="page-link"
-				disabled={!next}
-				style={{
-					borderTopLeftRadius: "0.35rem",
-					borderBottomLeftRadius: "0.35rem",
-					borderTopRightRadius: "0.35rem",
-					borderBottomRightRadius: "0.35rem",
-				}}
-				onClick={() => nextPage && onPageChange(nextPage)}
-				>
-					Next
-				</button>
-			</li>
+				<li className={`page-item`}>
+					<button className="page-lk"
+					style={{
+						// cursor: next ? "pointer" : "not-allowed",
+						borderTopRightRadius: "1rem",
+						borderBottomRightRadius: "1rem",
+						borderTopLeftRadius: "0.35rem",
+						borderBottomLeftRadius: "0.35rem",
+						fontStyle: 'italic',
+					}}>
+						Of {total_pages}
+					</button>
+				</li>
 
-			<li className={`page-item`}>
-				<button className="page-lk"
-				style={{
-					// cursor: next ? "pointer" : "not-allowed",
-					borderTopRightRadius: "1rem",
-					borderBottomRightRadius: "1rem",
-					borderTopLeftRadius: "0.35rem",
-					borderBottomLeftRadius: "0.35rem",
-					fontStyle: 'italic',
-				}}>
-					Of {total_pages}
-				</button>
-			</li>
-
-		</ul>
+			</ul>}
+		</>
 	);
 }
 
