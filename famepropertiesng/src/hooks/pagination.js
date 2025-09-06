d5afb16bba0ecb2422eb5d8b428c56f67731f5fa
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-06 13:12:46 +0100

    pagination.js: added spinner for loading states

--- famepropertiesng/src/hooks/pagination.js
+++ famepropertiesng/src/hooks/pagination.js
@@ -1,9 +1,30 @@
-import { Fragment } from "react";
+import { useState, useEffect, useRef, Fragment } from "react";
+import { BouncingDots } from "../spinners/spinner";
 
 function Pagination({ pagination, onPageChange }) {
-	const { count, next, prev, total_pages,
+	const { count, next, prev, total_pages, load
 		// page_range, start_index, end_index
 	} = pagination;
+	const loadRef = useRef(null);
+	const [loading, setLoading] = useState({
+										action: false,
+										mode: null,
+									});
+	useEffect(() => {
+		// console.log(
+		// 	'\nloadRef:', loadRef.current,
+		// 	'\nload:', load,
+		// 	'\nloading:', loading
+		// )
+		if (loading && loadRef.current!==load) {
+			// console.log('Load changed, stop loading spinner');
+			setLoading({
+				action: false,
+				mode: null,
+			});
+			loadRef.current = load;
+		}
+	}, [load])
 
 	// Extract current page from next/prev URLs
 	const getPageNumber = (url, mode) => {
@@ -15,28 +36,40 @@ function Pagination({ pagination, onPageChange }) {
 
 	const nextPage = getPageNumber(next, '+');
 	const prevPage = getPageNumber(prev, '-');
-	// console.log({ prevPage, nextPage, total_pages,
+	// console.log({ prevPage, nextPage, total_pages, load
 	// 	// page_range, start_index, end_index
 	// });
+	// console.log({loading})
 
 	return (
 		<>
 			{total_pages>1&&
 			<ul className="pagination">
 				{/* Previous */}
-				<li className={`page-item ${!prev ? "disabled" : ""}`}>
+				<li className={`d-flex page-item ${!prev ? "disabled" : ""}`}>
 					<button
 					className="page-link"
-					disabled={!prev}
+					disabled={!prev||(loading.action&&loading.mode==='-')}
 					style={{
 						borderTopLeftRadius: "1rem",
 						borderBottomLeftRadius: "1rem",
 						borderTopRightRadius: "0.35rem",
 						borderBottomRightRadius: "0.35rem",
 					}}
-					onClick={() => prevPage && onPageChange(prevPage)}
+					onClick={() => {
+						setLoading({
+							action: true,
+							mode: '-',
+						});
+						prevPage && onPageChange(prevPage);
+					}}
 					>
-						Prev
+						{(loading.action&&loading.mode==='-')?
+						<span className="d-flex align-items-center">
+							<BouncingDots size={"ts"} color={"#475569"} p={"0"} />
+						</span>
+						:
+						'Prev'}
 					</button>
 				</li>
 
@@ -57,19 +90,30 @@ function Pagination({ pagination, onPageChange }) {
 				</Fragment>
 
 				{/* Next */}
-				<li className={`page-item ${!next ? "disabled" : ""}`}>
+				<li className={`d-flex page-item ${!next ? "disabled" : ""}`}>
 					<button
 					className="page-link"
-					disabled={!next}
+					disabled={!next||(loading.action&&loading.mode==='+')}
 					style={{
 						borderTopLeftRadius: "0.35rem",
 						borderBottomLeftRadius: "0.35rem",
 						borderTopRightRadius: "0.35rem",
 						borderBottomRightRadius: "0.35rem",
 					}}
-					onClick={() => nextPage && onPageChange(nextPage)}
+					onClick={() => {
+						setLoading({
+							action: true,
+							mode: '+',
+						});
+						nextPage && onPageChange(nextPage);
+					}}
 					>
-						Next
+						{(loading.action&&loading.mode==='+')?
+						<span className="d-flex align-items-center">
+							<BouncingDots size={"ts"} color={"#475569"} p={"0"} />
+						</span>
+						:
+						'Next'}
 					</button>
 				</li>
 
@@ -83,7 +127,7 @@ function Pagination({ pagination, onPageChange }) {
 						borderBottomLeftRadius: "0.35rem",
 						fontStyle: 'italic',
 					}}>
-						Of {total_pages}
+						of {total_pages}
 					</button>
 				</li>
 
