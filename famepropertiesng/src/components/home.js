b3552bd0b80291722029207f0b05f47ef9a252fd
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-25 23:01:10 +0100

    home.js: added session check to keep user logged in (under 5days) and logged out after 5 days

--- famepropertiesng/src/components/home.js
+++ famepropertiesng/src/components/home.js
@@ -2,8 +2,10 @@ import React from "react"
 import { Carousel } from "./sections/carousel"
 import { Products } from "./products/products"
 import { BouncingDots } from "../spinners/spinner"
+import { useCheckLoginValidity } from "../hooks/checkLoginValidity"
 
 function Home() {
+	useCheckLoginValidity() // check login validity on home page load
 	const [loading,] = React.useState(true);
 	return (
 		<>
