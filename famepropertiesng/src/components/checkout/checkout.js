096d35c6fac12cdbb4f59f014be5694569d39547
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-06 00:46:23 +0100

    checkout.js: disabled place order button when required field and location are not filled (implemented location fields check from custom component) and removed logs

--- famepropertiesng/src/components/checkout/checkout.js
+++ famepropertiesng/src/components/checkout/checkout.js
@@ -43,7 +43,7 @@ const getField = (obj, field) => {
 }
 const baseURL = getBaseURL();
 const apiUrl = getBaseURL(true) + '/get-paystack-keys/pk/';
-console.log({baseURL, apiUrl})
+// console.log({baseURL, apiUrl})
 const shipping = 1500
 
 const paymentOptions = [
@@ -58,7 +58,7 @@ function Checkout() {
 	const [typedInstallAmount, setTypedInstallAmount] = useState(0);
 	const [finalInstallmentAmount, setFinalInstallmentAmount] = useState(null);
 	const [isInstallPlan, setIsInstallPlan] = useState(false);
-	const { cscFormData, CountryCompSelect, StateCompSelect, CityCompSelect } = useCountryStateCity();
+	const { cscFormData, cscRequiredFieldsGood, CountryCompSelect, StateCompSelect, CityCompSelect } = useCountryStateCity();
 	const { createLocal, createSession } = useCreateStorage()
 	const deviceSpec = useDeviceType();
 	const [isMounting, setIsMounting] = useState(true);
@@ -490,12 +490,13 @@ function Checkout() {
 		...checkoutResp,
 		amount: finalInstallmentAmount??(checkoutResp?.amount),
 	}
+	const checkJustFields = isFieldsValid({formData});
 	console.log({userInfo})
 	// console.log({isLoggedIn})
 	// console.log({allFieldsArr})
 	// console.log({allowedFieldsArr})
 	// console.log({phoneCode})
-	// console.log({shipToDifferent})
+	console.log({shipToDifferent})
 	// console.log({formData})
 
 	// console.log({country, state, city})
@@ -836,6 +837,11 @@ function Checkout() {
 								})}
 								<button
 								type="submit"
+								disabled={
+									(shipToDifferent?
+										(!checkJustFields||!cscRequiredFieldsGood):
+										false)
+								}
 								className="btn btn-block btn-primary font-weight-bold py-3">
 									Place Order
 								</button>
