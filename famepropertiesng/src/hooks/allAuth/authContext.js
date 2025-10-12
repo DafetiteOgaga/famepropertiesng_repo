b86dea31dc8cc1ff7d805b2800dc9e134bc160ee
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 16:43:01 +0100

    authContext.js: cart.js: removed logs and unused codes

--- famepropertiesng/src/hooks/allAuth/authContext.js
+++ famepropertiesng/src/hooks/allAuth/authContext.js
@@ -1,6 +1,4 @@
 import { createContext, useState, useContext, useEffect, useRef } from "react";
-// import { useCreateStorage } from "../setupLocalStorage";
-import { useRotStorage } from "./storeRot";
 
 // 1. Create the context
 export const AuthContext = createContext();
@@ -8,10 +6,8 @@ export const AuthContext = createContext();
 // 2. Create a provider component
 export function AuthProvider({ children }) {
 	const storedRot = useRef(false);
-	// const localRot = useRotStorage();
-	// console.log('##### localRot from useRotStorage:', localRot);
-	// const { createLocal } = useCreateStorage();
-	// ✅ Store access token in state (in-memory only)
+
+	// Store access token in state (in-memory only)
 	const [accessToken, setAccessToken] = useState(null);
 	const [refreshToken, setRefreshToken] = useState(null);
 	const [userInfo, setUserInfo] = useState(null);
@@ -34,15 +30,10 @@ export function AuthProvider({ children }) {
 	};
 
 	if (!storedRot.current) {
-		// console.log('Fetching rot number from localStorage...');
 		const storedNum = localStorage.getItem("fpng-rot");
 		const storedChars = localStorage.getItem("fpng-rchars");
-		// console.log('storedRot.current:', storedRot.current);
-		// console.log('storedChars:', storedChars);
-		// console.log('storedNum:', storedNum);
 		storedRot.current = storedNum && storedChars
 		if (storedRot.current) {
-			// console.log('Found rot in localStorage');
 			setRotNumber(parseInt(storedNum));
 			setStoredrChars(storedChars);
 		}
@@ -53,21 +44,11 @@ export function AuthProvider({ children }) {
 	const space = ['¶ ¾', '¾ ¶']
 	function RotCipher(text, shift) {
 		const cipherShift = rotNumber * shift;
-		// console.log(
-		// 	'shift:', shift,
-		// 	'- rotNumber:', rotNumber,
-		// 	'- cipherShift:', cipherShift
-		// );
+
 		if (!text) return text;
 		// Define the character set
-
 		const charset = "!#$%&*+-¹²³€½ŧ←↓→øþæßðđŋħˀĸł«»µ─¢/0123456789<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz|~";
-		// localStorage.setItem("hdChars", charset);
-		// const charset = storedChars
-		// console.log('storedChars inside RotCipher:', !!storedChars, storedChars.length);
-
 		const len = charset.length;
-		// console.log('charset length:', len);
 
 		// Compute the effective cipherShift value
 		const effectiveShift = ((cipherShift % len) + len) % len;
@@ -80,8 +61,6 @@ export function AuthProvider({ children }) {
 		}
 
 		// Transform the input text
-		// console.log('Original text:', text);
-		// console.log('storedRot.current:', storedRot.current);
 		try {
 			return Array.from(text)
 			.map(char => charMap[char] || (char===space[0]?space[1]:char===space[1]?space[0]:char))
@@ -90,28 +69,6 @@ export function AuthProvider({ children }) {
 			console.log('Error:', e)
 		}
 	}
-
-	// useEffect(() => {
-	// 	// On mount, load tokens from localStorage if they exist
-	// 	const storedAccess = createLocal.getItem("fpng-acc");
-	// 	const storedRefresh = createLocal.getItem("fpng-ref");
-	// 	const storedUser = createLocal.getItem("fpng-user");
-
-	// 	if (storedAccess) {
-	// 		setAccessToken(storedAccess);
-	// 	}
-	// 	if (storedRefresh) {
-	// 		setRefreshToken(storedRefresh);
-	// 	}
-	// 	if (storedUser) {
-	// 		try {
-	// 			setUserInfo(JSON.parse(storedUser));
-	// 		} catch (e) {
-	// 			console.error("Error parsing user info from localStorage:", e);
-	// 			setUserInfo(null);
-	// 		}
-	// 	}
-	// }, [accessToken, refreshToken, userInfo])
 
 	return (
 		<AuthContext.Provider value={{
