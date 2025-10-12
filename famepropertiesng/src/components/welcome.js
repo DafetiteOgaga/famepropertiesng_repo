878697465039793886a973b77dce81088ba3a837
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 16:42:32 +0100

    welcome.js: cart.js: removed logs and unused codes

--- famepropertiesng/src/components/welcome.js
+++ famepropertiesng/src/components/welcome.js
@@ -2,8 +2,6 @@ import { Breadcrumb } from './sections/breadcrumb';
 import { titleCase } from '../hooks/changeCase';
 
 function Welcome() {
-	// const location = useLocation().pathname.split("/").pop();
-	// const [products, setProducts] = useState([]);
 	return (
 		<>
 			{/* Breadcrumb Start */}
@@ -16,19 +14,7 @@ function Welcome() {
 				}}>
 				<h2
 				style={{color: '#475569',}}>{titleCase('welcome onboard!')}</h2>
-				{/* <h2
-				className="text-uppercase"
-				style={{color: '#475569',}}>Page Not Found</h2> */}
 			</div>
-			{/* <div className="container-fluid"
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				}}>
-				<h2
-				className="text-uppercase"
-				style={{color: '#475569',}}>Page Not Found</h2>
-			</div> */}
 		</>
 	)
 }
