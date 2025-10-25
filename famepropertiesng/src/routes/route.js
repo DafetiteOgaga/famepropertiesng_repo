bb0ff80bb1698cd72c2e1e4b7839e97498b7312e
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-25 20:04:06 +0100

    route.js: added and grouped routs into protected, public but prevent access to login and sign up if login and general public routes and another for only logged in persons

--- famepropertiesng/src/routes/route.js
+++ famepropertiesng/src/routes/route.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from './ProtectedRoute';
+import { PublicRoute } from './PublicRoute';
 import { Contact } from '../components/contact';
 import { Home } from '../components/home';
 import { Index } from '../components';
@@ -7,6 +9,7 @@ import { Cart } from '../components/checkout/cart';
 import { Checkout } from '../components/checkout/checkout';
 import { Detail } from '../components/productDetails/detail';
 import { PageNotFound } from '../components/pageNotFound';
+import { Unauthorised } from '../components/Unauthorised';
 import { Products } from '../components/products/products';
 import { LogIn } from '../components/loginSignUpProfile/login';
 import { SignUp } from '../components/loginSignUpProfile/signUpSetup/signUp';
@@ -33,11 +36,53 @@ function AppRoutes() {
 				{/* This makes Home the default route */}
 				<Route index element={<Home />} />
 
-				{/* tests */}
-				{/* <Route path="ng" element={<NigeriaLocationPicker />} /> */}
-				{/* <Route path="loc" element={<useCountryStateCity />} /> */}
+				{/* every other routes are from Outlet in Index */}
 
-				{/* Other routes from Outlet in Index */}
+				{/* Protected routes (auth + match check) */}
+				<Route element={<ProtectedRoute requireMatch />}>
+					{/* edit product */}
+					<Route path=":userID/:storeID/update-product/:productID" element={<EditProduct />} />
+					<Route path="products/:category/:userID/:storeID/update-product/:productID" element={<EditProduct />} />
+					{/* admin dashboard */}
+					<Route path="profile/:userID/admin-dashboard" element={<AdminDashboard />} />
+					{/* process image page */}
+					{/* <Route path="process-image" element={<ProcessImage />} /> */}
+					{/* store sign up */}
+					<Route path="profile/:userID/register-store" element={<StoreSignUp />} />
+					{/* post product from store */}
+					<Route path="post-products/:userID" element={<PostProduct />} />
+					{/* profile */}
+					<Route path="profile/:userID" element={<Profile />} />
+					{/* installment pay */}
+					<Route path="profile/:userID/installmental-payment" element={<InstallmentalPayment />} />
+					{/* installment pay success */}
+					<Route path="profile/:userID/installmental-payment/success" element={<Success />} />
+					{/* pay on delivery */}
+					<Route path="profile/:userID/pay-on-delivery" element={<PODelivery />} />
+					{/* pay on delivery success */}
+					<Route path="profile/:userID/pay-on-delivery/success" element={<Success />} />
+					{/* store products */}
+					<Route path="profile/:userID/store-products/:storeID" element={<StoreProducts />} />
+					{/* notifications */}
+					<Route path="notifications/:userID" element={<Notifications />} />
+					{/* staff dashboard */}
+					<Route path="staff-dashboard/:userID" element={<StaffDashboard />} />
+				</Route>
+
+				{/* Protected routes (auth only) */}
+				<Route element={<ProtectedRoute />}>
+					{/* <Route path="profile" element={<Profile />} /> */}
+				</Route>
+
+				{/* Public routes (login and sign up) */}
+				<Route element={<PublicRoute />}>
+					{/* log in */}
+					<Route path="login" element={<LogIn />} />
+					{/* sign up */}
+					<Route path="signup" element={<SignUp />} />
+				</Route>
+
+				{/* other Public routes */}
 				{/* products */}
 				<Route path="products/:productname" element={<Products />} />
 				{/* products/detail */}
@@ -45,51 +90,19 @@ function AppRoutes() {
 				{/* detail */}
 				<Route path="detail/:id" element={<Detail />} />
 				<Route path="products/:category/detail/:id" element={<Detail />} />
-				{/* edit product */}
-				<Route path=":userID/update-product/:productID" element={<EditProduct />} />
-				<Route path="products/:category/:userID/update-product/:productID" element={<EditProduct />} />
-				{/* admin dashboard */}
-				<Route path="profile/admin-dashboard" element={<AdminDashboard />} />
-				{/* process image page */}
-				<Route path="process-image" element={<ProcessImage />} />
 				{/* cart */}
 				<Route path="cart" element={<Cart />} />
-				{/* store sign up */}
-				<Route path="profile/register-store/:id" element={<StoreSignUp />} />
 				{/* welcome */}
 				<Route path="welcome" element={<Welcome />} />
 				{/* checkout success */}
 				<Route path="cart/checkout/success" element={<Success />} />
-				{/* post product from store */}
-				<Route path="post-products/:id" element={<PostProduct />} />
-				{/* profile */}
-				<Route path="profile" element={<Profile />} />
-				{/* installment pay */}
-				<Route path="profile/installmental-payment" element={<InstallmentalPayment />} />
-				{/* installment pay success */}
-				<Route path="profile/installmental-payment/success" element={<Success />} />
-				{/* pay on delivery */}
-				<Route path="profile/pay-on-delivery" element={<PODelivery />} />
-				{/* pay on delivery success */}
-				<Route path="profile/pay-on-delivery/success" element={<Success />} />
-				{/* store products */}
-				<Route path="profile/store-products/:storeID" element={<StoreProducts />} />
-				{/* log in */}
-				<Route path="login" element={<LogIn />} />
-				{/* sign up */}
-				<Route path="signup" element={<SignUp />} />
 				{/* checkout */}
 				<Route path="cart/checkout" element={<Checkout />} />
 				{/* contact us */}
 				<Route path="contact" element={<Contact />} />
-				{/* notifications */}
-				<Route path="notifications/:id" element={<Notifications />} />
-				{/* staff dashboard */}
-				<Route path="staff-dashboard/:id" element={<StaffDashboard />} />
-				{/* notification - staff dashboard
-				<Route path="staff-dashboard/:id/:nId" element={<StaffDashboard />} /> */}
 
-				{/* page not found */}
+				{/* page not found and unauthorised */}
+				<Route path="unauthorised" element={<Unauthorised />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Route>
 		</Routes>
